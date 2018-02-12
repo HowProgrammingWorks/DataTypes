220cabaec2cfd59e390ec2c2fadd80851474c0fc
tshemsedinov <timur.shemsedinov@gmail.com>
2018-02-12 23:55:19 +0200

    Improve examples

--- JavaScript/6-parse.js
+++ JavaScript/6-parse.js
@@ -18,11 +18,11 @@ console.log(parseInt(Infinity, 10)); // NaN
 console.log(parseInt('Infinity', 10)); // NaN
 console.log(parseInt('-Infinity', 10)); // NaN
 
-console.log(parseInt(5.1, 10)); // 5
-console.log(parseInt(5.1 * 1e50, 10)); // 5
-console.log(parseInt(0.000000000005, 10)); // 5
-console.log(parseInt(0.0000005, 10)); // 5
-console.log(parseInt(0.000005, 10)); // 0
+console.log(5.1,            parseInt(5.1, 10)); // 5
+console.log(5.1 * 1e50,     parseInt(5.1 * 1e50, 10)); // 5
+console.log(0.000000000005, parseInt(0.000000000005, 10)); // 5
+console.log(0.0000005,      parseInt(0.0000005, 10)); // 5
+console.log(0.000005,       parseInt(0.000005, 10)); // 0
 
 // 255
 console.log(parseInt('fF', 16));
--- JavaScript/8-bitwise.js
+++ JavaScript/8-bitwise.js
@@ -8,7 +8,7 @@ const aBinary = a.toString(2);
 const bBinary = b.toString(2);
 const cBinary = c.toString(2);
 
-console.log(a + ' to base 2: ' + aBinary);
+console.log(a + ' to base 2:  ' + aBinary);
 console.log(b + ' to base 2: ' + bBinary);
 
 console.log('Bitwise operators');
