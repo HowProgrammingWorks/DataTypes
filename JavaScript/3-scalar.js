86f8a46e3715f2f2e10cd25b45136c61ac2a91a2
tshemsedinov <timur.shemsedinov@gmail.com>
2018-09-17 02:05:01 +0300

    Improve example

--- JavaScript/3-scalar.js
+++ JavaScript/3-scalar.js
@@ -1,17 +1,12 @@
 'use strict';
 
-const scalar1 = 5;
-const scalar2 = scalar1;
+let scalar1 = 5;
+let scalar2 = scalar1;
+scalar1--;
+scalar2++;
+console.dir({ scalar1, scalar2 });
 
-const object1 = { field: scalar1 };
+const object1 = { field: 5 };
 const object2 = object1;
-
-console.dir({ object1 });
-console.dir({ object2 });
-console.dir({ scalar1 });
-
 object1.field = 6;
-
-console.dir({ object1 });
-console.dir({ object2 });
-console.dir({ scalar1 });
+console.dir({ object1, object2 });
