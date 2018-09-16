1ea3d295b52c688ec4228e8ce8ddaa562a97e4c7
tshemsedinov <timur.shemsedinov@gmail.com>
2018-09-17 02:05:20 +0300

    Fix typeof

--- JavaScript/4-types.js
+++ JavaScript/4-types.js
@@ -22,15 +22,15 @@ cities.unshift('New York');
 console.log('shift: ' + cities.shift());
 console.log('pop: ' + cities.pop());
 
-console.log({ i }, typeof(i));
-console.log({ s }, typeof(s));
-console.log({ b }, typeof(b));
-console.log({ f }, typeof(f));
+console.log({ i }, typeof i);
+console.log({ s }, typeof s);
+console.log({ b }, typeof b);
+console.log({ f }, typeof f);
 
-console.log({ person }, typeof(person));
+console.log({ person }, typeof person);
 console.log({ isArray: Array.isArray(person) });
 
-console.log({ cities }, typeof(cities));
+console.log({ cities }, typeof cities);
 console.log({ isArray: Array.isArray(cities) });
 
 console.log({ instanceofArray: cities instanceof Array });
--- JavaScript/5-undefined-null-NaN.js
+++ JavaScript/5-undefined-null-NaN.js
@@ -2,20 +2,20 @@
 
 // undefined
 let emptyScalar;
-console.log({ emptyScalar }, typeof(emptyScalar));
+console.log({ emptyScalar }, typeof emptyScalar);
 
 // null
 const emptyObject = null;
-console.log({ emptyObject }, typeof(emptyObject));
+console.log({ emptyObject }, typeof emptyObject);
 
 // NaN
 let count = NaN;
-console.log({ count }, typeof(count));
+console.log({ count }, typeof count);
 
 count = undefined + 1;
 console.dir({ count });
 
-console.log(Infinity, -Infinity, typeof(Infinity));
+console.log(Infinity, -Infinity, typeof Infinity);
 
 const s = (
   emptyObject === null ?
