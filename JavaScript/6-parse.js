e2d51b4665f8ac2872e08a781a15843bb632c0e2
kulia26 <kulinich261@gmail.com>
2019-07-03 16:14:00 +0300

    Error fixed and comment added

--- JavaScript/6-parse.js
+++ JavaScript/6-parse.js
@@ -18,11 +18,11 @@ console.log(parseInt(Infinity, 10)); // NaN
 console.log(parseInt('Infinity', 10)); // NaN
 console.log(parseInt('-Infinity', 10)); // NaN
 
-console.log(5.1,            parseInt(5.1, 10)); // 5
-console.log(5.1 * 1e50,     parseInt(5.1 * 1e50, 10)); // 5
-console.log(0.000000000005, parseInt(0.000000000005, 10)); // 5
-console.log(0.0000005,      parseInt(0.0000005, 10)); // 5
-console.log(0.000005,       parseInt(0.000005, 10)); // 0
+console.log(5.1,            parseInt(5.1, 10)); // 5.1 5
+console.log(5.1 * 1e50,     parseInt(5.1 * 1e50, 10)); // 5.1e+50 5
+console.log(0.000000000005, parseInt(0.000000000005, 10)); // 5e-12 5
+console.log(0.0000005,      parseInt(0.0000005, 10)); // 5e-7 5
+console.log(0.000005,       parseInt(0.000005, 10)); // 0.000005 0
 
 // 255
 console.log(parseInt('fF', 16));
@@ -76,6 +76,6 @@ console.log(parseFloat('(5)')); // NaN
 console.log(parseFloat('"5"')); // NaN
 console.log(parseFloat('[5]')); // NaN
 
-console.log(parseFloat('NaN')); // Infinity
+console.log(parseFloat('NaN')); // NaN
 console.log(parseFloat('Infinity')); // Infinity
-console.log(parseFloat('-Infinity')); // Infinity
+console.log(parseFloat('-Infinity')); // -Infinity
