c89195ef32adc67bc2e6eadb5787695d85ec5a96
Pienskoi <vladymyrpenskoy@gmail.com>
2019-09-11 16:00:54 +0300

    Exercises 1-4 completed

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = array => {
+  const hash = {};
+  for (const item of array)
+    isNaN(hash[typeof item]) ?
+      hash[typeof item] = 1 :
+      hash[typeof item]++;
+  return hash;
+};
 
 module.exports = { countTypesInArray };
