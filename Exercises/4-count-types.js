6f7566c5011cb9f94bec9f0d7eab53d73b952af1
Stan <stanislav.soloview@gmail.com>
2019-09-13 00:35:49 +0300

    Replaced if statement

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -4,11 +4,8 @@ const countTypesInArray = array => {
   const types = {};
   for (const el of array) {
     const type = typeof(el);
-    if (type in types) {
-      types[type]++;
-    } else {
-      types[type] = 1;
-    }
+    types[type] = types[type] || 0;
+    types[type]++;
   }
 
   return types;
