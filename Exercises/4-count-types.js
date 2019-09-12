6fd28a73cd89782523a0ed49bd83c003e7a4ec38
Sergei Komarov <komarovsmail33@gmail.com>
2019-09-12 19:15:32 +0300

    Fix all tests

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,16 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = (arr, hash = {}) => {
+  arr.forEach(el => {
+    const type = typeof el;
+    if (Object.keys(hash).includes(type)) {
+      ++hash[type];
+    } else {
+      hash[type] = 1;
+    }
+  });
+
+  return hash;
+};
 
 module.exports = { countTypesInArray };
