a193f2763e506653501a5e182558462dd79930e6
Kathrin <kathrin.shpigel.90@gmail.com>
2019-09-13 23:10:46 +0300

    end 4-count-types.js

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const countTypesInArray = null;
-
+const countTypesInArray = array => {
+  const hash = {};
+  for (const value of array) {
+    const type = typeof value;
+    hash[type] ? hash[type]++ : hash[type] = 1;
+  }
+  return hash;
+};
 module.exports = { countTypesInArray };
