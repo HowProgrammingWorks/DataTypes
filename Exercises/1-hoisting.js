5051af33b1b039a1b01e74f2b151157fe85b7512
Pavlo Pavlenko <alskarf@gmail.com>
2019-09-13 20:18:13 +0300

    solution numb4 - refactored

--- Exercises/1-hoisting.js
+++ Exercises/1-hoisting.js
@@ -1,5 +1,8 @@
 'use strict';
 
-const fn = null;
+const fn = () => {
+  const someVariable = null;
+  return someVariable;
+};
 
 module.exports = { fn };
--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const countTypesInArray = null;
-
+const countTypesInArray = list => {
+  const obj = {};
+  for (const item of list) {
+    const instance = typeof item;
+    (instance in obj) ?
+      (obj[instance] += 1) :
+      (obj[instance] = 1);
+  }
+  return obj;
+};
 module.exports = { countTypesInArray };
