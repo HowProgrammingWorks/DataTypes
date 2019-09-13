dee8bdf0cbbe4117672dbd11bcca0c5da0096db5
Oleg Vintoniuk <oleg.vintonuk@gmail.com>
2019-09-13 22:48:40 +0300

    complete tasks 1, 2, 3, 4

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = arr => {
+  const hesh = { };
+
+  for (const name of arr) {
+    const t = typeof name;
+    (t === 'boolean' || 'string' || 'number') && (hesh[t] = (hesh[t] || 0) + 1);
+  }
+  return hesh;
+};
 
 module.exports = { countTypesInArray };
