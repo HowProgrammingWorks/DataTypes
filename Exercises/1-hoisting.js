5157aa81d82075838bf8a83395cc1a1089a10431
Ponkin Dmitrij <ponkin.dmitrij@gmail.com>
2019-09-13 13:15:01 +0300

    Labs tasks

--- Exercises/1-hoisting.js
+++ Exercises/1-hoisting.js
@@ -1,5 +1,8 @@
 'use strict';
 
-const fn = null;
+const fn = () => {
+  const test = 5;
+  console.log(test);
+};
 
 module.exports = { fn };
--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = array => {
+  const hash = {};
+
+  for (const elem of array) {
+    hash[typeof elem] = (hash[typeof elem]) + 1 || 1;
+  }
+
+  return hash;
+};
 
 module.exports = { countTypesInArray };
