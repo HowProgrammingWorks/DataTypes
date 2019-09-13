33d917785f7ca7e525c781496246d6595c117376
Mark Prykhoda <hroscko@gmail.com>
2019-09-13 22:57:33 +0300

    Complete types exercise

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,15 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = arr => {
+  const types = {};
+  for (const el of arr) {
+    const t = el === null ? 'null' : typeof el;
+    if (!(t in types)) {
+      types[t] = 0;
+    }
+    types[t] += 1;
+  }
+  return types;
+};
 
 module.exports = { countTypesInArray };
