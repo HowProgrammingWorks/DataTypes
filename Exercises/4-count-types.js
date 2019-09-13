f6d14314d85da9d7b77285155f2c444c0ccbc4bc
Станіслав Кулічковський <staskulch@localhost.localdomain>
2019-09-13 21:25:04 +0300

    Add change in 4 lab

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,25 +1,13 @@
 'use strict';
 
-
-
-const array = [23, 'wor', 75, true, -34, { s: 1, z: 2 }, 'aga', 'false', false];
-
-
-const object = {
-  number: 0,
-  string: 0,
-  boolean: 0,
-  object: 0,
-};
-for (const variable of array) {
-  object[typeof variable]++;
-}
-
 const countTypesInArray  = funObjects => {
   const object2 = {};
   for (const ar of funObjects) {
-    object2[typeof ar] ? object2[typeof ar]++ : object2[typeof ar] = 1;
+    if (typeof ar in object2)
+      object2[typeof ar]++;
+    else object2[typeof ar] = 1;
   }
   return object2;
 };
 module.exports = { countTypesInArray };
+
