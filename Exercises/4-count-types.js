deb23c5ca6221707c698529c316489d86053ab16
Leonidius20 <leonidslupsky2002@gmail.com>
2019-09-14 20:56:34 +0300

    Implementations

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,30 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = array => {
+  const output = {};
+
+  for (const entry of array) {
+    const type = typeof entry;
+    if (type in output) output[type]++;
+    else output[type] = 1;
+  }
+
+  return output;
+};
+
+const exampleArray = [
+  true,
+  'string',
+  12.6,
+  BigInt(12000),
+  { field: 'value' },
+  [ 'cats', 'dogs', false ],
+  false,
+  234444444444,
+  'b',
+  { x: 5, y: 4, z: 5 }
+];
+
+console.log(countTypesInArray(exampleArray));
 
 module.exports = { countTypesInArray };
