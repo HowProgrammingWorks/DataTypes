411221cedfb45c892b435a7ad8ddb8bf40ad5b2c
Konstantin Gorshkov <const.potter@gmail.com>
2019-09-14 18:52:34 +0600

    Complete fourth task

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,23 @@
 'use strict';
 
-const countTypesInArray = null;
+// const trash = ['string', true, 'another_string', false, 'number', [], '333',
+//   333, true, 22, false, 0, '0', 1, '', {}, [], true, {}, []];
+
+// const countTypesInArray = arr => {
+//   const types = { number: 0, string: 0, boolean: 0, object: 0 }; //
+//   for (const el of arr) {
+//     types[typeof el]++;
+//   }
+//   return types;
+// };
+
+const countTypesInArray = arr => {
+  const types = {};
+  for (const el of arr) {
+    const type = typeof el;
+    types[type] = types[type] ? types[type] + 1 : 1;
+  }
+  return types;
+};
 
 module.exports = { countTypesInArray };
