d1c9ea90064ca41f6ec25fedac59bbe2cfa215b7
Evgeniy Sedov <sedoff-online@yandex.ru>
2019-09-15 00:18:46 +0700

    DataTypes task 4 part 1

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,17 @@
 'use strict';
 
-const countTypesInArray = null;
+const arr = [true, 'hello', 5, 12, -200, false, false, 'word'];
+const typesObject = { number: 0, string: 0, boolean: 0 };
 
-module.exports = { countTypesInArray };
+const countTypesInArray = elements => {
+  for (const element of elements) {
+    if (typeof element === 'number') typesObject.number += 1;
+    if (typeof element === 'string') typesObject.string += 1;
+    if (typeof element === 'boolean') typesObject.boolean += 1;
+  }
+  return typesObject;
+};
+
+countTypesInArray(arr);
+
+// module.exports = { countTypesInArray };
