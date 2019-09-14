9cd5bc75469504182eac34e515d43ced5416c4ee
ArMANIAK <ArMANIAK@localhost.localdomain>
2019-09-14 23:46:37 +0300

    Task 4 was corrected

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,16 +1,19 @@
 'use strict';
 
-/* const arr = [true, 3, '4', false, { n: 3 }, function (x) { return x + 1; },
-  { name: 'Marc' }, 'Marc', 3.11, [1, 2, 4], 5];*/
+const arr = [true, 3, '4', false, { n: 3 }, function (x) { return x + 1; },
+  { name: 'Marc' }, 'Marc', 3.11, [1, 2, 4], 5];
 
 const countTypesInArray = arr => {
   const result = {};
   for (const item of arr) {
-    const temp = typeof(item);
-    if (result[temp]) result[temp]++;
-    else result[temp] = 1;
+    const type = typeof item;
+    const count = result[type];
+    if (count) result[type] = count + 1;
+    else result[type] = 1;
   }
   return result;
 };
+
+console.dir(countTypesInArray(arr));
 
 module.exports = { countTypesInArray };
