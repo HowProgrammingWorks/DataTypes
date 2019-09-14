aff95294e793d2673e2ec0dbe15f2241f08af2a5
danielshulgin <danielshulgin@users.noreply.github.com>
2019-09-14 12:47:31 +0300

    lab tasks 1, 2, 3, 4

--- Exercises/1-hoisting.js
+++ Exercises/1-hoisting.js
@@ -1,5 +1,5 @@
 'use strict';
 
-const fn = null;
+const fn = () => console.log('OK');
 
 module.exports = { fn };
--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,10 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = typeArray => {
+  const hash = {};
+  for (const Value of typeArray)
+    hash[typeof(Value)] = typeof(Value) in hash ? hash[typeof(Value)] + 1 : 1;
+  return hash;
+};
 
 module.exports = { countTypesInArray };
