3f73f8ca5bea1c46851b63222cc6d0abe42d84b6
Alexander Storchak <alexander.s.storchak@gmail.com>
2019-09-15 20:21:54 +0300

    Changed the solution for DataTypes/Hoisting to non-hoisting vaiable.

--- Exercises/1-hoisting.js
+++ Exercises/1-hoisting.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const fn = () => {
-  //const hoisted = 'blablabla';
+  const hoisted = 'blablabla';
   console.log(`Hoisted var is "${hoisted}".`);
-  var hoisted = 1;
+  //var hoisted = 1;
 };
 
 module.exports = { fn };
