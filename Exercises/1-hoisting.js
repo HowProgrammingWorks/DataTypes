04ab388aa3bd9cb98fbd0ca72134316d50049179
OleksandrLevak <sashavytvyckyj@gmail.com>
2019-09-15 13:34:19 +0300

    Labs tasks 1,2,3,4

--- Exercises/1-hoisting.js
+++ Exercises/1-hoisting.js
@@ -1,5 +1,12 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-var */
+
 'use strict';
 
-const fn = null;
-
+const fn = () => {
+  xy = 5;
+  xy++;
+  var xy;
+};
 module.exports = { fn };
--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = Arr => {
+  const h = { };
+  for (const t of Arr) {
+    const m =  typeof t;
+    h[m] = h[m] || 0;
+    h[m]++;
+  }
+  return h;
+};
 
 module.exports = { countTypesInArray };
