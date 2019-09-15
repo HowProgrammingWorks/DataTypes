37a311d376dc97dbff85f6599a8619c573c866bb
Antiselekotin <deksazom8@gmail.com>
2019-09-15 20:31:42 +0300

    Karlovskyi Vova

--- Exercises/3-by-reference.js
+++ Exercises/3-by-reference.js
@@ -1,5 +1,8 @@
 'use strict';
 
-const inc = null;
+const inc = a => {
+  a.n++;
+  return undefined;
+};
 
 module.exports = { inc };
--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,15 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = array => {
+  const object = {};
+  for (const i of array) {
+    if (object[typeof i]) {
+      ++object[typeof i];
+    } else {
+      object[typeof i] = 1;
+    }
+  }
+  return object;
+};
 
 module.exports = { countTypesInArray };
