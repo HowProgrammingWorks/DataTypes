95287a0d191384b2292ac12b57f16bbaffbb384b
ra111page <venom808@gmail.com>
2019-09-15 13:04:34 +0300

    Completed ex 1,2,3,4

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = array => {
+  const answer = {};
+  for (const element of array) {
+    const type = typeof element;
+    isNaN(answer[type]) ?  answer[type] = 1 : answer[type] += 1;
+  }
+  return answer;
+};
 
 module.exports = { countTypesInArray };
