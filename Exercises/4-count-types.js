0bf2b85d8f6c83f9dbc8de00b8fe6da476ca39e8
Станіслав Кулічковський <staskulch@localhost.localdomain>
2019-09-15 12:22:02 +0300

    Add change in 8 line in 4 lad

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -4,8 +4,8 @@ const countTypesInArray  = arg => {
   const result = {};
   for (const value of arg) {
     const field = typeof value;
-    if (field in result)
-      result[field]++;
+
+    if (field in result) result[field]++;
     else result[field] = 1;
   }
   return result;
