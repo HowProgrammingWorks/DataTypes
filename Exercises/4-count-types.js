29b72a0c5f0bd61b496c03206d437c5a9d50f2e7
01f <01f@gmail.com>
2019-09-15 13:56:55 +0300

    Labs tasks 1,2,3,4

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = values => {
+  const types = {};
+  for (const value of values) {
+    (value === null) ? types[null] = ++types[null] || 1 :
+      types[typeof value] = ++types[typeof value] || 1;
+  }
+  return types;
+};
 
 module.exports = { countTypesInArray };
