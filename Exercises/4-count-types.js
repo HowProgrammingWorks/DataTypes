3ee8059fe6e1172bce80dd254d4a740eabc77a01
arthur2406 <rizhiy2406@gmail.com>
2019-09-16 16:28:58 +0300

    labs tasks 1,4

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = arr => {
+  const hash = {};
+  for (const o of arr) {
+    const type = typeof o;
+    hash[type] = ++hash[type] || 1;
+  }
+  return hash;
+};
 
 module.exports = { countTypesInArray };
