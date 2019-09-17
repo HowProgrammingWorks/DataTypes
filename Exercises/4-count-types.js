d45eb041f60ecc93da6d0b02ca183a1f1e5f7ca7
Eugene Artamonov <evzheniy@yandex.ru>
2019-09-17 10:36:56 +0300

    Use if instead ternary operator

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -4,9 +4,10 @@ const countTypesInArray = array => {
   const result = {};
   for (const item of array) {
     const type = typeof item;
-    const count = result[type];
-    result[type] = count ?
-      count + 1 : 1;
+    if(result[type]) {
+      result[type] = 0;
+    }
+    result[type]++;
   }
   return result;
 };
