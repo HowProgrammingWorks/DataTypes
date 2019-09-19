ae1167ad77b24f6c10e55a2b812b86649128c9a0
Nick Everhard <everhard@fex.net>
2019-09-19 14:08:21 +0300

    Исправляет выполнение задания 1

--- Exercises/1-hoisting.js
+++ Exercises/1-hoisting.js
@@ -1,10 +1,8 @@
 'use strict';
 
 const fn = () => {
+  const hoisting = 'Hello world!';
   console.log(hoisting);
-  hoisting = 'Hello world!';
-  console.log(hoisting);
-  var hoisting;
 };
 
 module.exports = { fn };
