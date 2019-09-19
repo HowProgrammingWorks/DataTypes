a8739df13ffd0c6935b3cf9c64054bcd68b8450a
Nick Everhard <everhard@fex.net>
2019-09-19 14:03:47 +0300

    Добавляет выполнение заданий

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = arr => {
+  const result = {};
+  for (const item of arr) {
+    const type = typeof item;
+    if (!result[type]) result[type] = 0;
+    result[type]++;
+  }
+  return result;
+};
 
 module.exports = { countTypesInArray };
