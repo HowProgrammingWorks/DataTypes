45d6410fd3d06038e78ec00ff8d9c4e12b66792c
alexandr.pavletsov <alexandr.pavletsov@rcntec.com>
2019-09-25 00:49:31 +0500

    Labs 1-4. But hoisted(!)

--- Exercises/1-hoisting.js
+++ Exercises/1-hoisting.js
@@ -1,5 +1,9 @@
 'use strict';
 
-const fn = null;
+const fn = () => {
+  console.log(rizedVar);
+  //use var 4 use hoist. const and let not supported by standard...
+  var rizedVar = 'Use before declare, but it udef';
+};
 
 module.exports = { fn };
--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,13 @@
 'use strict';
-
-const countTypesInArray = null;
-
+const countTypesInArray = arr => {
+  const dynTypes = {};
+  for (const itm of arr) {
+    const t = typeof itm;
+    if (!(t in dynTypes)) {
+      dynTypes[(t)] = 0;
+    }
+    dynTypes[(t)] += 1;
+  }
+  return dynTypes;
+};
 module.exports = { countTypesInArray };
