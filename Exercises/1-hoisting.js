9cb79a5b2eb91fdb318d3b6063001cfc480e11e8
Nikolai Klevanski <n.klevanskiy@avratec.com>
2019-09-25 18:11:32 +0300

    Labs 1-4

--- Exercises/1-hoisting.js
+++ Exercises/1-hoisting.js
@@ -1,5 +1,8 @@
 'use strict';
 
-const fn = null;
+const fn = () => {
+  const str = 'Nick';
+  return str;
+};
 
 module.exports = { fn };
--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = arr => {
+  const hash = {};
+  for (const i of arr) {
+    const type = typeof i;
+    hash[type] ? hash[type]++ : hash[type] = 1;
+  }
+  return hash;
+};
 
 module.exports = { countTypesInArray };
