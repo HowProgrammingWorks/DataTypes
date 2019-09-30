1ee95b2d62825146bfa250840e1f70a60f33df2c
hannanezrivnanna <kicka44@gmail.com>
2019-09-30 18:50:57 +0300

    Fixed after code review

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,12 +1,13 @@
 'use strict';
 
-const countTypesInArray = () => {
-  const array = [true, 'hello', 5, 12, -200, false, false,
-    'word', { name: 'Nikita', age: 15 }, NaN, undefined, undefined];
+const array = [true, 'hello', 5, 12, -200, false, false,
+  'word', { name: 'Nikita', age: 15 }, NaN, undefined, undefined];
+
+const countTypesInArray = (arr = array) => {
   const hash = {};
-  for (const element of array) {
-    const type = typeof(element);
-    hash[type] ? ++hash[(type)] : hash[(type)] = 1;
+  for (const element of arr) {
+    const t = typeof(element);
+    hash[t] ? ++hash[(t)] : hash[(t)] = 1;
   }
   return hash;
 };
