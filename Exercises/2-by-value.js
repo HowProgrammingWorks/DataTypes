56b91633e9c336442d5f7bbb17bb1fdd1ac32037
Viktor.Orlyk <victor.orlyk93@gmail.com>
2019-10-01 10:29:54 +0300

    corrections, code clean-up

--- Exercises/2-by-value.js
+++ Exercises/2-by-value.js
@@ -1,11 +1,5 @@
 'use strict';
 
 const inc = number => ++number;
-const a = 5;
-const b = inc(a);
-console.log({ a, b });
-
-
-
 
 module.exports = { inc };
--- Exercises/3-by-reference.js
+++ Exercises/3-by-reference.js
@@ -4,10 +4,4 @@ const inc = object => {
   object.n++;
 };
 
-const obj = { n: 5 };
-
-inc(obj);
-
-console.dir(obj);
-
 module.exports = { inc };
