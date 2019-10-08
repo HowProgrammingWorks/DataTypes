f814cc67817c8770bff62580d94c6007e49da00d
Igor Kas <kasigor555@gmail.com>
2019-10-08 19:43:41 +0300

    Добавлены решения задач с 1-й по 4-ю.

--- Exercises/3-by-reference.js
+++ Exercises/3-by-reference.js
@@ -1,5 +1,17 @@
 'use strict';
+/**
+ * Подготовьте функцию inc с сигнатурой inc(num: Num),
+ * где Num является объектом с полем n,
+ * чтобы функция изменила поле исходного объекта переданного по ссылке,
+ * пример вызова const obj = { n: 5 }; inc(obj); console.dir(obj);
+ */
 
-const inc = null;
+const inc = obj => {
+  obj.n++;
+};
+
+// const obj = { n: 5 };
+// inc(obj);
+// console.dir(obj);
 
 module.exports = { inc };
