df71fcfe5fc9dc35b7559fb43f9f737cc0f2b458
Vladyslav Tkachenko <tkachenko94ua@gmail.com>
2019-10-10 11:21:47 +0000

    Fixed tasks 3 and 4

--- Exercises/3-by-reference.js
+++ Exercises/3-by-reference.js
@@ -4,8 +4,4 @@ const inc = obj => {
   obj.n++;
 };
 
-const obj = { n: 5 };
-inc(obj);
-console.dir(obj);
-
 module.exports = { inc };
--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,12 +1,12 @@
 'use strict';
 
-
 const countTypesInArray = array => {
   const hash = {};
   for (const element of array) {
-    const elementType = typeof(element);
-    hash[elementType] ? ++hash[elementType] : hash[elementType] = 1;
+    const elementType = typeof element;
+    hash[elementType] = hash[elementType] ? ++hash[elementType] : 1;
   }
   return hash;
 };
+
 module.exports = { countTypesInArray };
