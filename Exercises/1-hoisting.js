ae80075fe33b2bafc694211b5844ae5384792b1f
Tamila <tamila@localhost.localdomain>
2019-10-18 02:03:19 +0300

    Labs tasks 1-4

--- Exercises/1-hoisting.js
+++ Exercises/1-hoisting.js
@@ -1,5 +1,9 @@
 'use strict';
 
-const fn = null;
+const fn = () => {
+  const index = 20;
+  console.log(index);
+};
 
+fn();
 module.exports = { fn };
--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = arr => {
+  const types = {};
+  for (const i of arr) {
+    if (!types[typeof i]) types[typeof i] = 0;
+    types[typeof i]++;
+  }
+  return types;
+};
 
 module.exports = { countTypesInArray };
