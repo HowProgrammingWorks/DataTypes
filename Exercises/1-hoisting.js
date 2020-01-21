3caff4a7e32f7417772f1fe3a4f0eab0f1703abf
Hranat Ivan <e4drcf@gmail.com>
2020-01-21 16:51:18 +0100

    labs 1-4 done

--- Exercises/1-hoisting.js
+++ Exercises/1-hoisting.js
@@ -1,5 +1,8 @@
 'use strict';
 
-const fn = null;
+const fn = () => {
+  console.log({ a });
+  var a = 5;
+};
 
 module.exports = { fn };
--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = arr => {
+  const result = {};
+  for (const element of arr) {
+    const type = typeof(element);
+    const count = result[type] || 0;
+    result[type] = count + 1;
+  }
+  return result;
+};
 
 module.exports = { countTypesInArray };
