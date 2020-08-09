693d0c91a3bb953c50bb77796d828ba92af80fc5
Alex <Sa6aFed@localhost.localdomain>
2020-08-09 18:15:14 +0300

    Labs tasks 1, 2, 3, 4

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,15 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = arr => {
+  const dict = {};
+  for (const value of arr) {
+    if (typeof value in dict) {
+      dict[typeof value] += 1;
+    } else {
+      dict[typeof value] = 1;
+    }
+  }
+  return dict;
+};
 
 module.exports = { countTypesInArray };
