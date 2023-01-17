5cc11cfad41a85cd8f4d58efd8c82d826c50a3ed
Tima Gixe <timagixe@protonmail.com>
2023-01-17 08:50:46 +0200

    Solves 4-count-types

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = (types) => {
+  const hash = {};
+  for (const value of types) {
+    const type = typeof value;
+    hash[type] = hash[type] ? hash[type] + 1 : 1;
+  }
+  return hash;
+};
 
 module.exports = { countTypesInArray };
