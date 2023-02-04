9ee784b01fae4342ef3acd7da6ea0f564fc94072
kcholombytko <funnygungan@gmail.com>
2023-02-05 01:08:57 +0200

    implement types count solution

---
+++ Solutions-Python/3-count-types.py
@@ -0,0 +1,11 @@
+def countTypes(arr: list) -> dict:
+	counters = dict()
+	for el in arr:
+		_type = type(el).__name__
+		counters[_type] = counters.get(_type, 0) + 1
+	return counters
+
+arr = [True, 'hello', 5, 12, -200, False, False, 'word']
+res = countTypes(arr)
+
+print(res)
