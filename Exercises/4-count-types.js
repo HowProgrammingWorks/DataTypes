b453cd310676dc85665b12597f81856c1859f756
LiamLimann <117228545+LiamLimann@users.noreply.github.com>
2023-06-10 20:28:18 +0300

    Labs tasks #1m, #2, #3, #4.

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = (arr) => {
+  const counters = {};
+  for (const element of arr) {
+    const type = typeof element;
+    const count = counters[type] || 0;
+    counters[type] = count + 1;
+  }
+  return counters;
+};
 
 module.exports = { countTypesInArray };
